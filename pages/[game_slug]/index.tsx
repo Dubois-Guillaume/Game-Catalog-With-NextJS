f1cfd303962c9e087af1ff16a50048e8c194163f
Guillaume Dubois <duboisguillaume08@gmail.com>
2022-03-25 16:17:40 +0100

    Display name and cover

--- pages/[game_slug]/index.tsx
+++ pages/[game_slug]/index.tsx
@@ -2,15 +2,15 @@ import { GetServerSideProps } from "next";
 import Image from "next/image";
 
 const DisplayGameInformation: React.FC<{ gameInfo }> = ({ gameInfo }) => {
-  console.log("gameInfo", gameInfo);
+  // console.log("gameInfo", gameInfo);
 
   return (
     <div>
       <p>{gameInfo.name}</p>
       <Image
-        width={`${gameInfo.game_screenshots[0].width}`}
-        height={`${gameInfo.game_screenshots[0].height}`}
-        src={`${gameInfo.game_screenshots[0].url}`}
+        width={`${gameInfo.cover_width}`}
+        height={`${gameInfo.cover_height}`}
+        src={`${gameInfo.cover_url}`}
         alt="cover du jeux"
       />
     </div>
@@ -25,7 +25,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   );
   const gameInfo = await response.json();
 
-  console.log("gameInfo", gameInfo);
+  // console.log("gameInfo", gameInfo);
 
   return {
     props: {
